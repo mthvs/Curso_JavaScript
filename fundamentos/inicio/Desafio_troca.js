13d61d565fc7e30e19a86173a57788f65fd7b80d
mthvs <matheusfoz@outlook.com>
2023-09-12 10:54:04 -0300

    mandando atividades

---
+++ fundamentos/inicio/Desafio_troca.js
@@ -0,0 +1,13 @@
+let a = 7;
+let b = 94;
+let aux = 0;
+
+aux= a;
+a = b;
+b = aux;
+
+//metodo de troca diferente
+//[a, b] = [b, a]
+
+console.log("o valor de A = "+a);
+console.log("o valor de B = "+b);
