34680b9d18d5259ca43f580d9992524ee7f4fef7
mthvs <matheusfoz@outlook.com>
2024-05-17 09:45:53 -0300

    atividades feitas

---
+++ fundamentos/inicio/desafio_Areas_Circuferencia.js
@@ -0,0 +1,4 @@
+let raio = 10;
+let area_circulo = Math.PI*Math.pow(raio,2);
+
+console.log("valor da area do circulo: "+area_circulo+"m2")
---
+++ fundamentos/inicio/desafio_troca_valores.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94;
+let aux;
+
+
+console.log("Valor de A: " + a);
+console.log("Valor de B: " + b);
+
+aux = a;
+a = b;
+b = aux;
+
+console.log("Valor de A trocado: " + a);
+console.log("Valor de B trocado: " + b);
